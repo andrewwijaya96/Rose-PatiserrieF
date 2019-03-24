359df4d0a2f7782597df82fc1d8703683a346fc8
Andrew Wijaya <yohanesandrew483@gmail.com>
2019-03-24 20:05:00 +0700

    Commit Awij 8.05 24 Maret 2019

---
+++ nav.js
@@ -0,0 +1,21 @@
+function goToHome(){
+  window.location = "index.html";
+}
+function goToCart(){
+  window.location = "cart.html";
+}
+function goToAboutUs(){
+  window.location = "about.html";
+}
+function goToCake(){
+  window.location = "cake.html";
+}
+function goToChoux(){
+  window.location = "choux.html";
+}
+function goToAbcInfo(){
+  window.location = "abc.html";
+}
+function goToSwcInfo(){
+  window.location = "swc.html";
+}
