49c66ecd63d94b739ded779e047ccfe78e9ef8fa
Andrew Wijaya <yohanesandrew483@gmail.com>
2019-03-25 19:54:32 +0700

    Commit Awij 19.54 25 Maret 2019
    Final sebelum revisi

--- checkout.js
+++ checkout.js
@@ -1,5 +1,7 @@
-var nama = document.getElementById("nama").value;
-var telp = document.getElementById("telp").value;
-var alamat = document.getElementById("alamat").value;
-var email = document.getElementById("email").value;
-var bayar = document.getElementById("bayar").checked;
+function checkout(){
+  clearCart();
+  window.location = "receipt.html";
+}
+function back(){
+  window.location = "index.html";
+}
